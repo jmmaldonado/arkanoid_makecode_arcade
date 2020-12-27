944a5f1b76851103dedbbe8cf7b99ec082b412cf
jmmaldonado <jmmaldonado@users.noreply.github.com>
2020-12-27 11:14:48 +0100

    Cambiado el game update para que no tenga pausa

--- main.ts
+++ main.ts
@@ -28,15 +28,18 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, ot
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.brick, function (sprite, otherSprite) {
     info.changeScoreBy(1)
+    if (info.score() == (filas + 1) * (columnas + 1)) {
+        game.over(true)
+    }
     if (sprite.y > otherSprite.y) {
-        direccion_y = 1
+        direccion_y = 0.7
     } else {
-        direccion_y = -1
+        direccion_y = -0.7
     }
     if (sprite.x > otherSprite.x) {
-        direccion_x = 1
+        direccion_x = 0.7
     } else {
-        direccion_x = -1
+        direccion_x = -0.7
     }
     otherSprite.destroy()
 })
@@ -49,6 +52,8 @@ function sacar_bola () {
 let direccion_x = 0
 let direccion_y = 0
 let ladrillo: Sprite = null
+let columnas = 0
+let filas = 0
 let bola: Sprite = null
 let nave: Sprite = null
 scene.setBackgroundColor(1)
@@ -78,19 +83,16 @@ bola = sprites.create(img`
     . f f . 
     `, SpriteKind.Projectile)
 nave.y = 110
-let filas = 4
-let columnas = 7
+filas = 4
+columnas = 7
 construir_muro(filas, columnas)
 info.setLife(3)
 info.setScore(0)
 controller.moveSprite(nave, 100, 0)
 sacar_bola()
-game.onUpdateInterval(25, function () {
+game.onUpdate(function () {
     bola.x += direccion_x
     bola.y += direccion_y
-    if (info.score() == (filas + 1) * (columnas + 1)) {
-        game.over(true)
-    }
     if (bola.y >= 110) {
         info.changeLifeBy(-1)
         if (info.life() == 0) {
@@ -100,12 +102,12 @@ game.onUpdateInterval(25, function () {
         }
     }
     if (bola.y <= 2) {
-        direccion_y = 1
+        direccion_y = 0.7
     }
     if (bola.x >= scene.screenWidth() - 2) {
-        direccion_x = -1
+        direccion_x = -0.7
     }
     if (bola.x <= 2) {
-        direccion_x = 1
+        direccion_x = 0.7
     }
 })
